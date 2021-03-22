4ca8c88d8e1c082f5dc122091528b795258d9816
GHOSU1012 <ghosu1012@gmail.com>
2021-03-22 04:56:54 -0300

    added file to npm install everything

---
+++ npminstall.js
@@ -0,0 +1,29 @@
+var fs = require('fs')
+var resolve = require('path').resolve
+var join = require('path').join
+var cp = require('child_process')
+var os = require('os')
+
+// get library path
+var lib = resolve(__dirname)
+
+fs.readdirSync(lib)
+  .forEach(function (mod) {
+    var modPath = join(lib, mod)
+    // ensure path has package.json
+    if (!fs.existsSync(join(modPath, 'package.json'))) {
+      return
+    }
+
+    // npm binary based on OS
+    var npmCmd = os.platform().startsWith('win') ? 'npm.cmd' : 'npm'
+
+    // install folder
+    cp.spawn(npmCmd, ['i'], {
+      env: process.env,
+      cwd: modPath,
+      stdio: 'inherit'
+    })
+  })
+
+  //src: https://stackoverflow.com/a/31774097
